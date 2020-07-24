da36fb8d8583de8d33a41519e21e73c6f7cbf9b9
alvalfric <56022145+alvalfric@users.noreply.github.com>
2020-07-24 19:19:38 +0200

    Uploading D03

---
+++ src/main/population/create-schema.sql
@@ -0,0 +1,199 @@
+
+    create table `administrator` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `alferez_bulletin` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `type` varchar(255),
+        `url` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `anonymous` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `authenticated` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `challenge` (
+       `id` integer not null,
+        `version` integer not null,
+        `average_goal` varchar(255),
+        `average_reward` varchar(255),
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `expert_goal` varchar(255),
+        `expert_reward` varchar(255),
+        `rookie_goal` varchar(255),
+        `rookie_reward` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `consumer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `customization_parameter` (
+       `id` integer not null,
+        `version` integer not null,
+        `spam_threshold` double precision,
+        `spam_words_english` varchar(255),
+        `spam_words_spanish` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `inquirie` (
+       `id` integer not null,
+        `version` integer not null,
+        `body` varchar(255),
+        `contact_email` varchar(255),
+        `creation` datetime(6),
+        `deadline` datetime(6),
+        `max_money_amount` double precision,
+        `max_money_currency` varchar(255),
+        `min_money_amount` double precision,
+        `min_money_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `notice` (
+       `id` integer not null,
+        `version` integer not null,
+        `body` varchar(512),
+        `creation` datetime(6),
+        `deadline` datetime(6),
+        `header_picture` varchar(255),
+        `related_link1` varchar(255),
+        `related_link2` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `overture` (
+       `id` integer not null,
+        `version` integer not null,
+        `body` varchar(255),
+        `contact_email` varchar(255),
+        `creation` datetime(6),
+        `deadline` datetime(6),
+        `max_money_amount` double precision,
+        `max_money_currency` varchar(255),
+        `min_money_amount` double precision,
+        `min_money_currency` varchar(255),
+        `title` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `provider` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `shout` (
+       `id` integer not null,
+        `version` integer not null,
+        `author` varchar(255),
+        `moment` datetime(6),
+        `text` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `technology_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `activity_sector` varchar(255),
+        `description` varchar(255),
+        `email` varchar(255),
+        `inventor_name` varchar(255),
+        `source_type` varchar(255),
+        `stars` integer,
+        `title` varchar(255),
+        `website` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `tool_record` (
+       `id` integer not null,
+        `version` integer not null,
+        `activity_sector` varchar(255),
+        `description` varchar(255),
+        `email` varchar(255),
+        `inventor_name` varchar(255),
+        `source_type` varchar(255),
+        `stars` integer,
+        `title` varchar(255),
+        `website` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `user_account` (
+       `id` integer not null,
+        `version` integer not null,
+        `enabled` bit not null,
+        `identity_email` varchar(255),
+        `identity_name` varchar(255),
+        `identity_surname` varchar(255),
+        `password` varchar(255),
+        `username` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `hibernate_sequence` (
+       `next_val` bigint
+    ) engine=InnoDB;
+
+    insert into `hibernate_sequence` values ( 1 );
+
+    alter table `user_account` 
+       add constraint UK_castjbvpeeus0r8lbpehiu0e4 unique (`username`);
+
+    alter table `administrator` 
+       add constraint FK_2a5vcjo3stlfcwadosjfq49l1 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `anonymous` 
+       add constraint FK_6lnbc6fo3om54vugoh8icg78m 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `authenticated` 
+       add constraint FK_h52w0f3wjoi68b63wv9vwon57 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `consumer` 
+       add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `provider` 
+       add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
